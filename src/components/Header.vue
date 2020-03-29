df66c973813d4615aaff45dcfb0c288ea17e3b74
Emma <emmaswleung@gmail.com>
2020-03-29 11:01:31 +0800

    add logo

---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div>
+      <h3 class='headerLeft'>THE MARINE JOURNAL</h3>
+      <h5 class='headerLeft'>The Beginner's Guide to Marine Ecology</h5>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.headerLeft {
+  text-align:left;
+  padding-left: 5px;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
