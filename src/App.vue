3d9ba4c6903c9ce03d87ef8150b8239c2334f224
Emma <emmaswleung@gmail.com>
2020-03-29 12:10:12 +0800

    Added menu

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,16 @@
 </template>
 
 <script>
-import Header from './components/Header.vue'
+import Header from './components/Header.vue';
 
 export default {
   name: 'App',
   components: {
-    Header
-  }
-}
+    Header,
+  },
+};
+
+
 </script>
 
 <style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,22 @@
 <template>
   <div>
-    <div>
-      <h3 class='headerLeft'>THE MARINE JOURNAL</h3>
-      <h5 class='headerLeft'>The Beginner's Guide to Marine Ecology</h5>
+    <img id='logo' src='../assets/whale-logo.png' alt='Logo'/>
+    <div style='float: left'>
+      <h2 class='headerLeft'>THE MARINE JOURNAL</h2>
+      <h4 class='headerLeft'>The Beginner's Guide to Marine Ecology</h4>
+    </div>
+    <div style='float: right'>
+      <md-menu md-direction="bottom-start">
+        <md-button md-menu-trigger>Bottom Start</md-button>
+
+        <md-menu-content>
+          <md-menu-item>My Item 1</md-menu-item>
+          <md-menu-item>My Item 2</md-menu-item>
+          <md-menu-item>My Item 3</md-menu-item>
+        </md-menu-content>
+      </md-menu>
+      <h4 class="headerLeft2">Home</h4>
+      <h4 class="headerLeft2">Explore</h4>
     </div>
   </div>
 </template>
@@ -10,7 +24,8 @@
 <script>
 export default {
   name: 'Header',
-}
+};
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -18,7 +33,20 @@ export default {
 
 .headerLeft {
   text-align:left;
-  padding-left: 5px;
+  padding-left: 10px;
+  margin: 0;
+  padding-top: 7px;
+}
+
+.headerLeft2{
+  float: left;
+  padding: 5px;
+}
+
+#logo {
+  width: 90px;
+  height: 90px;
+  float: left;
 }
 
 ul {
