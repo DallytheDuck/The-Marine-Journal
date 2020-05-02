a68d43d43c7ad4002e1ee6d5168996d54f9474c9
Emma <emmaswleung@gmail.com>
2020-05-02 15:57:04 +0800

    Completed Overview section

--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -1,13 +1,13 @@
 /* eslint-disable max-len */
 <template>
-    <div>
+    <div id="bigbox">
         <div>
-            <p>What You can Find</p>
+            <p id="smalltitle">What You can Find</p>
         </div>
-        <div>
-            <img src="../assets/seafloor-vector.png" alt="Seafloor Vector by WIX">
+        <div class="section">
+            <img class="vector" src="../assets/seafloor-vector.png" alt="Seafloor Vector by WIX">
             <div>
-                <p>THE OCEAN FLOOR</p>
+                <p class="subtitles">THE OCEAN FLOOR</p>
             </div>
             <div>
                 <p>The bottom of the sea ... is it flat? Lumpy? Does it have hills?
@@ -15,10 +15,10 @@
                     plate boundaries, features of the deep ocean floor, and continental margins.</p>
             </div>
         </div>
-        <div>
-            <img src="../assets/anchor-vector.png" alt="Anchor Vector by WIX">
+        <div class="section">
+            <img class="vector" src="../assets/anchor-vector.png" alt="Anchor Vector by WIX">
             <div>
-                <p>WATER</p>
+                <p class="subtitles">WATER</p>
             </div>
             <div>
                <p>This is your item description. Use this space to add a description of the services,
@@ -26,10 +26,10 @@
                     Easily turn any item into a full page by clicking ‘Create a page from this item’ in the edit panel.</p>
             </div>
         </div>
-        <div>
-            <img src="../assets/whale-vector.png" alt="Whale Vector by WIX">
+        <div class="section">
+            <img class="vector" src="../assets/whale-vector.png" alt="Whale Vector by WIX">
             <div>
-                <p>OCEAN ANIMALS</p>
+                <p class="subtitles">OCEAN ANIMALS</p>
             </div>
             <div>
                 <p>This is your item description. Use this space to add a description of the services, 
@@ -38,7 +38,7 @@
             </div>
         </div>
         <div>
-            <p>Explore</p>
+            <button id="explorebutton">Explore</button>
         </div>
     </div>
 </template>
@@ -55,4 +55,34 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.subtitles {
+    font-size: 20px;
+    margin-top: 0px;
+}
+.vector {
+    width: 70px;
+    height: 70px;
+}
+.section {
+    width: 20vw;
+    float: left;
+    padding: 6.5vw;
+}
+#bigbox {
+    background-color: rgb( 245, 247, 250);
+}
+#smalltitle {
+    padding-top:70px;
+    font-size: 20px;
+}
+#explorebutton {
+    border-color: #2c3e50;
+    border-width: 1px;
+    border-style: solid;
+    font-size: 18px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    background-color: rgb( 245, 247, 250);
+    padding: 7px, 5px, 7px, 5px;
+    margin-bottom: 70px;
+}
 </style>
