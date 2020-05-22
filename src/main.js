e15166c147cb4a02c1fa98c52b7a10a9c93da328
RuChiang <jgchengru@gmail.com>
2020-05-22 14:32:52 +0800

    Added Vue css

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 import VueMaterial from 'vue-material';
+import 'vue-material/dist/vue-material.css';
 
 import App from './App.vue';
 
 
 Vue.use(VueMaterial);
+
 
 Vue.config.productionTip = false;
 
@@ -13,10 +15,10 @@ new Vue({
 }).$mount('#app');
 
 
-/* 
+/*
   Router
   when given a url -> return the corresponding component
 
   mapping
-    '/' -> 
+    '/' ->
 */
